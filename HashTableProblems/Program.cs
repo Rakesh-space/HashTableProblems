8d02a6d774d53438a8540ad2681733006d68b60a
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-11 22:54:35 +0530

    Updating Class Name here

--- HashTableProblems/Program.cs
+++ HashTableProblems/Program.cs
@@ -7,8 +7,29 @@ namespace HashTableProblems
     {
         static void Main(string[] args)
         {
-            UC2MyHashCode<K, V>.counWordFrequency("Paranoids are notparanoid because they are paranoid butbecause they keep putting themselvesdeliberately into paranoid avoidable situations");
+            Console.WriteLine("Hello World!");
+            CountWordFrequency("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations");
             Console.ReadLine();
+        }
+        public static void CountWordFrequency(string sentence)
+        {
+            MyHashCode<string, int> hashtable = new MyHashCode<string, int>(10);
+            string[] words = sentence.Split(' ');
+            foreach (string word in words)
+            {
+                if (hashtable.Exists(word))
+                {
+                    hashtable.Add(word, hashtable.get(word) + 1);
+
+                }
+                else
+                {
+                    hashtable.Add(word, 1);
+                }
+            }
+            Console.WriteLine("Displaying after add operations");
+            hashtable.Display();
+            Console.WriteLine("=======================================");
         }
     }
 }
